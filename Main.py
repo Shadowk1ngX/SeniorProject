b956f62fedff584c92d64ab109c24c037bf7b504
Shadowk1ngX <94004343+Shadowk1ngX@users.noreply.github.com>
2025-04-12 21:41:17 -0400

    Main Update with threading

--- Main.py
+++ Main.py
@@ -2,4 +2,24 @@ import ChatbotGpt as Chat
 import FaceRec as Face
 import ObjectDetection as Object
 
+import threading
 
+
+
+def run_face_recognizer():
+    face = Face()
+    face.recognize_and_track()
+
+def run_object_detector():
+    detector = Object(confidence_threshold=0.5)
+    detector.detect_from_webcam()
+
+if __name__ == "__main__":
+    face_thread = threading.Thread(target=run_face_recognizer)
+    object_thread = threading.Thread(target=run_object_detector)
+
+    face_thread.start()
+    object_thread.start()
+
+    face_thread.join()
+    object_thread.join()
